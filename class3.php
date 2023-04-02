f640f9c62d6e48410ebe62bdb7933e6dadc75523
unknown <asalaminsikder787@gmail.com>
2023-04-02 17:30:29 +0600

    first commit

---
+++ class3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="class3.php" method="get" >
+        <label for="Fname">Fast Name</label>
+        <input type="text" id="Fname" name="Fname" valu=""><br><br>
+
+        <label for="Lname">Last Name</label>
+        <input type="text" id="Lname" name="Lname" valu=""><br><br>
+
+        <label for="Email">E-mail</label>
+        <input type="email" id="Email" name="Email" valu=""><br><br>
+
+        <label for="Password">Password</label>
+        <input type="Password" id="Password" name="Password" Valu><br><br>
+
+        <label for="Password2">Conform Password</label>
+        <input type="Password" id="Password2" name="Password2" valu=""><br><br>
+
+        <label for="number">Phone Number</label>
+        <input type="Number" id="Number" name="Number" valu=""><br><br>
+
+        <label for="">Gender</label> :
+        <input type="radio" id="radio" valu="">
+        <label for="radio">Male</label>
+        <input type="radio" id="radio" valu="">
+        <label for="radio">female</label><br>
+
+
+        <input type="submit" id="submit">
+
+
+
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+
+// function hoursToSeconds($h){
+//    return $h*3600;
+
+
+// }
+// echo hoursToSeconds(18).'s';
+// echo'<br>';
+
+// function isLessThen100($number){
+    
+//     if ($number<100){
+//         return 'true';
+//     }
+//     else {
+//         return 'false';
+//     }
+
+// }
+// echo isLessThen100(99);
+// echo'<br>';
+
+// function demo($number){
+//     return $number*7200;
+// }
+// echo demo(18);
+// echo'<br>';
+
+// function test($number){
+//     if($number>100){
+//         return 'true';
+//     }
+//     else{
+//         return 'false';
+//     }
+// }
+// echo test(44);
+// echo'<br>';
+
+?>
+
+<?php
+// $name = ['Rayhan','Reza'];
+
+// print_r($_REQUEST);
+
+// print_r($_REQUEST['name'] ?? 'Alamin Sikder');
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    HI' My Name Is  01716685764
+</h1>
+</body>
+
+
+<?php
+
+$Bazar_list=array("Rayhan"=>"35" ,"Shuvo"=>"30","Tamim"=>"52");
+echo $Bazar_list['Rayhan'];
+?>
